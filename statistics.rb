66a7cbd56793dea8248a12f5aea6f2d24f5d2e8e
CaptainJNS <sereda9623@gmail.com>
2019-05-26 13:00:59 +0300

    Declare statistics methods

--- statistics.rb
+++ statistics.rb
@@ -2,4 +2,12 @@
 
 # module for calculate statistics
 module Statistic
+  def top_reader(orders, quantity = 1)
+  end
+
+  def top_book(orders, quantity = 1)
+  end
+
+  def number_of_readers(orders, quantity = 3)
+  end
 end
